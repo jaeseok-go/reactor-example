8ee46338eb1df51cd3513637fe8f3962339128ab
jaeseok-go <gojeasuk2@naver.com>
2023-08-10 22:01:55 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/jaeseok/study/reactorexample/ReactorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jaeseok.study.reactorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
